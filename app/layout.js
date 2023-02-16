891754b8dbcb4db4dd6ba4587feb45c7cd63abac
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-02-16 18:04:21 +0200

    initial settings applied

--- app/layout.js
+++ app/layout.js
@@ -2,14 +2,7 @@ import '../styles/globals.css';
 
 const RootLayout = ({ children }) => (
   <html lang="en">
-    <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Tourney&display=swap"
-        rel="stylesheet"
-      />
-    </head>
+    <head></head>
     <body>{children}</body>
   </html>
 );
