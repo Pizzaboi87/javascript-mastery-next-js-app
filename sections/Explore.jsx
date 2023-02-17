5021011a253ee69bc10645d0eb0627787ef8c31d
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-02-17 22:05:49 +0200

    timing modification

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -3,17 +3,18 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 import styles from '../styles';
+import { exploreWorlds } from '../constants';
 import { staggerContainer } from '../utils/motion';
 import { ExploreCard, TitleText, TypingText } from '../components';
-import { exploreWorlds } from '../constants';
 
 const Explore = () => {
   const [active, setActive] = useState('world-3');
+
   return (
     <section className={`${styles.paddings}`} id="explore">
       <motion.div
         variants={staggerContainer}
-        initial="hiddne"
+        initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.25 }}
         className={`${styles.innerWidth} mx-auto flex flex-col`}
